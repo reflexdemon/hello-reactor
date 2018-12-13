4996f46a1f49c63127fe3dea9cc81b537c58849d
reflexdemon <venkateswara.vp@gmail.com>
2018-12-12 20:53:28 -0500

    Initial Commit

---
+++ src/test/java/io/vpv/reactive/helloreactor/HelloReactorApplicationTests.java
@@ -0,0 +1,17 @@
+package io.vpv.reactive.helloreactor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloReactorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
